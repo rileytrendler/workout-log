db6a3f318048368517ffe882de3def34812f1b7d
Riley T <rtrendler@wisc.edu>
2026-07-10 21:22:06 -0500

    Deploy workout log to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
+  base: "/workout-log/",
   plugins: [
     react(),
     VitePWA({
@@ -15,16 +16,16 @@ export default defineConfig({
         theme_color: "#ffffff",
         background_color: "#ffffff",
         display: "standalone",
-        start_url: "/",
-        scope: "/",
+        start_url: ".",
+        scope: ".",
         icons: [
           {
-            src: "/pwa-192x192.png",
+            src: "pwa-192x192.png",
             sizes: "192x192",
             type: "image/png"
           },
           {
-            src: "/pwa-512x512.png",
+            src: "pwa-512x512.png",
             sizes: "512x512",
             type: "image/png"
           }
@@ -32,9 +33,6 @@ export default defineConfig({
       },
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg}"]
-      },
-      devOptions: {
-        enabled: true
       }
     })
   ]
